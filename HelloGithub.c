f731a973676e1e22e5d625f41471b7d6ee7b1405
Duc Nguyen <nguyen@doukkunoMacBook-Pro.local>
2021-06-06 21:58:05 +0900

    Modify file

---
+++ HelloGithub.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+void s2112207_hello(void);
+void s2112208_hello(void);
+void s2112209_hello(void);
+void s2112210_hello(void);
+void s2112211_hello(void);
+void s2112212_hello(void);
+void s2112213_hello(void);
+void s2112214_hello(void);
+void s2112215_hello(void);
+
+int main(int argc, char const *argv[])
+{
+	s2112207_hello();
+	s2112208_hello();
+	s2112209_hello();
+	s2112210_hello();
+	s2112211_hello();
+	s2112212_hello();
+	s2112213_hello();
+	s2112214_hello();
+	s2112215_hello();
+	return 0;
+}
+
+void s2112207_hello(void){
+	printf(" \n");
+}
+
+void s2112208_hello(void){
+	printf(" \n");
+}
+
+void s2112209_hello(void){
+	printf(" \n");
+}
+
+void s2112210_hello(void){
+	printf(" \n");
+}
+
+void s2112211_hello(void){
+	printf(" \n");
+}
+
+void s2112212_hello(void){
+	printf(" \n");
+}
+
+void s2112213_hello(void){
+	printf(" \n");
+}
+
+void s2112214_hello(void){
+	printf(" \n");
+}
+
+void s2112215_hello(void){
+	printf(" \n");
+}
